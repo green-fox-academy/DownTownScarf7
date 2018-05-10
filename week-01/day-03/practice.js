49ee850e4e342d449d4a6aa980fbf01a247ead09
DownTownScarf7 <vbence741@gmail.com>
2018-05-10 08:49:56 +0200

    added js file with 3 tasks

---
+++ week-01/day-03/practice.js
@@ -0,0 +1,11 @@
+'use strict';
+console.log('Hello World!\n');
+
+// (hello-me) Modified to say my name
+console.log('Hello Bence!\n');
+
+// (humpty-dumpty) Reorganized
+console.log('Humpty Dumpty sat on a wall,\nHumpty Dumpty had a great fall.\nAll the king\'s horses and all the king\'s men\nCouldn\'t put Humpty together again.\n');
+
+// (hello-others) Greeting three classmates
+console.log('Hello Orsi!\nHello Sanyi!\nHello Nori\n')
