1410f5c66716cf48544b6dc34b9d53be4514855e
DownTownScarf7 <vbence741@gmail.com>
2018-05-10 10:35:01 +0200

    added coding-hours

--- week-01/day-03/practice.js
+++ week-01/day-03/practice.js
@@ -22,3 +22,15 @@ function qikmafs(num1, num2) {
 }
 qikmafs(13, 22);
 
+// (coding-hours) Calculating...
+function codingHours(hours, weeks, awh) {
+
+ // Hours spent coding in a semester
+    console.log(weeks * 5 * hours);
+
+ // Percentage of working hours
+    console.log( ((weeks * awh) / (weeks * 168) * 100).toFixed(2) + '%' );
+
+    return true;
+}
+codingHours(6, 17, 52);
