31c5370b34bd2626029c2eade1408fb83c3a6842
DownTownScarf7 <vbence741@gmail.com>
2018-05-10 16:52:47 +0200

    added not working draw_triangle.ts, yay!

---
+++ week-01/day-03/draw-triangle.ts
@@ -0,0 +1,24 @@
+'use strict';
+/*
+function triangle(star: string, lines: number) {
+  for (let i = lines; i < 1; i--) {
+    if (i == 1) {
+      return `${star}\n`;
+    } else {
+      console.log(triangle(star, lines));
+      return `${star}`;
+    }
+  }
+}
+console.log(triangle("*", 4));
+*/
+
+for (let i = 0; i < 4; i++) {
+  if (i == 1) {
+    console.log('*');
+  } else {
+      for (i ){
+        console.log('*\n');
+      }
+    }
+}
