bcfe1e0fa5bbcee992f877b108bda766028c444c
DownTownScarf7 <vbence741@gmail.com>
2018-05-14 22:19:14 +0200

    added simple-replace.ts

---
+++ week-02/day-02/simple-replace.ts
@@ -0,0 +1,9 @@
+export{};
+'use strict';
+
+let example:string = 'In a dishwasher far far away';
+
+example = example.replace(/dishwasher/i, 'galaxy');
+
+console.log(example);
+// export = example;  why is this even here?
