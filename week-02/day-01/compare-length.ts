7c9357b7d84043d8e6e44800a9c611b1ec418a0c
DownTownScarf7 <vbence741@gmail.com>
2018-05-14 12:08:12 +0200

    added compare-length.ts

---
+++ week-02/day-01/compare-length.ts
@@ -0,0 +1,11 @@
+export{};
+'use strict';
+
+let firstList: number[] = [1, 2, 3];
+let secondList: number[] = [4, 5];
+
+if (secondList.length > firstList.length) {
+  console.log('The second list is longer than the first one');
+} else {
+  console.log('The first list is longer than the second one');
+}
