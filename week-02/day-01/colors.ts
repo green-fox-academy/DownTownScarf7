51a15a3f385e3caccbfd3967c968bc964d8f468f
DownTownScarf7 <vbence741@gmail.com>
2018-05-14 14:13:16 +0200

    added colors.ts

---
+++ week-02/day-01/colors.ts
@@ -0,0 +1,10 @@
+export{};
+'use strict';
+
+let colors: string[][] = [
+  ['lime', 'forest green', 'olive', 'pale green', 'spring green'],
+  ['orange red', 'red', 'tomato'],
+  ['orchid', 'violet', 'pink', 'hot pink']
+];
+
+console.log(`${colors[0]}\n${colors[1]}\n${colors[2]}`);
