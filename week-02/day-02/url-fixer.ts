e825b2a366340c8a50417332ff47546c765bf64b
DownTownScarf7 <vbence741@gmail.com>
2018-05-14 23:07:11 +0200

    fixed the url... or did I?

--- week-02/day-02/url-fixer.ts
+++ week-02/day-02/url-fixer.ts
@@ -3,6 +3,7 @@ export{};
 
 // Fucking seriously...?
 let url:string = 'https//www.reddit.com/r/nevertellmethebots';
+url = 'https://www.reddit.com/r/nevertellmetheodds';
 function fuckThis(shittyUrl) {
   let dankUrl = url.slice(url.indexOf('www'), url.indexOf('/r/') + 3)
   return `https://${dankUrl}dankmemes/`;
