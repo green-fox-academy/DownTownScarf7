449d4bac307d2b8ec9c676f4b784ab8d3f25d67c
DownTownScarf7 <vbence741@gmail.com>
2018-05-14 17:27:14 +0200

    edited

--- week-02/day-01/unique.ts
+++ week-02/day-01/unique.ts
@@ -1,10 +1,11 @@
 export{};
 'use strict';
 
+function uniqueSort(value, index, self) { 
+  return self.indexOf(value) === index;
+}
+
 function unique(arr: number[]) { 
-  function uniqueSort(value, index, self) { 
-    return self.indexOf(value) === index;
-  }
   return arr.filter(uniqueSort);
 }
 console.log(unique([1, 11, 34, 11, 52, 61, 1, 34]))
