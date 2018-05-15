8595c9525623a138152209cdf8d8cc103487a5f1
DownTownScarf7 <vbence741@gmail.com>
2018-05-15 09:46:41 +0200

    added solar-system.ts

---
+++ week-02/day-02/solar-system.ts
@@ -0,0 +1,17 @@
+export{};
+'use strict';
+// Saturn is missing from the planetList
+// Insert it into the correct position
+// Create a function called putSaturn() which has list parameter and returns the correct list.
+// bonus for using some built in methods
+let planetList: string[] = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Uranus', 'Neptune'];
+
+function putSaturn(list){
+  list.push('Saturn');
+  return list;
+}
+
+// Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Saturn"
+console.log(putSaturn(planetList));
+
+//export = putSaturn;
