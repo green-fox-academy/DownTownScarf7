89ad52480dec77eff09b158c08a5894608660cf4
DownTownScarf7 <vbence741@gmail.com>
2018-05-15 10:15:17 +0200

    added matchmaking.ts

---
+++ week-02/day-02/matchmaking.ts
@@ -0,0 +1,20 @@
+export{};
+'use strict';
+// Write a function that joins two array by matching one girl with one boy in a new array
+// Exepected output: ["Eve", "Joe", "Ashley", "Fred"...]
+let girls: string[] = ['Eve', 'Ashley', 'Bözsi', 'Kat', 'Jane'];
+let boys: string[] = ['Joe', 'Fred', 'Béla', 'Todd', 'Neef', 'Jeff'];
+
+function makingMatches(list1, list2){
+  let madeInHeaven: string[] = list1;
+  let step: number = 0;
+  for (let i = 1; i <= (list1.length); i += 2) {
+    madeInHeaven.splice(i, 0, boys[step]);
+    step++
+  }
+  return madeInHeaven;
+}
+
+console.log(makingMatches(girls, boys));
+
+//export = makingMatches;
