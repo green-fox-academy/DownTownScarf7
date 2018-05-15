61a92629079ec45adda410eb5d633b9768ff6640
DownTownScarf7 <vbence741@gmail.com>
2018-05-15 10:21:11 +0200

    added append-letter.ts

---
+++ week-02/day-02/append-letter.ts
@@ -0,0 +1,15 @@
+export{};
+'use strict';
+// Create a function called "appendA()" that adds "a" to every string in the far list.
+// The parameter should be a list.
+let far: string[] = ['kuty', 'macsk', 'kacs', 'rók', 'halacsk'];
+
+function appendA(list:string[]):string[]{
+  list = list.map(ele => {ele += 'a'; return ele;});
+  return list;
+}
+
+console.log(appendA(far));
+
+// The output should be: 'kutya', 'macska', 'kacsa', 'róka', 'halacska'
+//export = appendA;
