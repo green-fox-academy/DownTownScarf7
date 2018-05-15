c56724ca471a10022a6fcbc56cda9dfc4a321c93
DownTownScarf7 <vbence741@gmail.com>
2018-05-15 16:31:00 +0200

    magical improvements

--- week-02/day-02/element-finder.ts
+++ week-02/day-02/element-finder.ts
@@ -5,7 +5,7 @@ export{};
 // if it contains return "Hoorray" otherwise return "Noooooo"
 const numbers: number[] = [1, 2, 3, 4, 5, 6, 8];
 function containsSeven(list: any){
-  if (list.indexOf(7) == true) {
+  if (list.indexOf(7)) {
     return 'Hoorray';
   } else {
     return 'Noooooo';
@@ -22,4 +22,4 @@ console.log(containsSeven(numbers));
 console.log(findElem(numbers, 7));
 // The output should be: "Noooooo"
 // Do this again with a different solution using different list functions!
-export = containsSeven;
+// export = containsSeven;
