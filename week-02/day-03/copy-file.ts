1a95d87bdef850fe08557c9eda273e52ddb66c15
DownTownScarf7 <vbence741@gmail.com>
2018-05-16 13:28:51 +0200

    added copy-file.ts

---
+++ week-02/day-03/copy-file.ts
@@ -0,0 +1,20 @@
+export{};
+'use strict';
+
+declare function require(path:string):any;
+
+function copyFile(fromFile:string, toFile:string):boolean{
+  const fs = require('fs');
+  try{
+    fs.createReadStream(fromFile).pipe(fs.createWriteStream(toFile));
+    return true;
+  }
+  catch{
+    return false;
+  }
+}
+if (copyFile('copy-file-from.txt', 'copy-file-to.txt')){
+  console.log('Done!');
+} else{
+  console.log('Fuck!');
+}
