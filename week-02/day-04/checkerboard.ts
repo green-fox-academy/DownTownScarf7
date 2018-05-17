f9cd9566eaa856474da6b1955f99bbd78b987a62
DownTownScarf7 <vbence741@gmail.com>
2018-05-17 13:34:01 +0200

    added checkerboard.ts & .html

---
+++ week-02/day-04/checkerboard.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Fill the canvas with a checkerboard pattern.
+let tgl: boolean = false, unlock: boolean = false;
+
+for (let j = 0; j <= 350; j += 50) {
+  for (let i = 0; i <= 550; i += 50) {
+    if (tgl && unlock) {
+      i = 25;
+      unlock = false;
+    }
+    ctx.fillRect(i * 2, j, 50, 50)
+  }
+  tgl = !tgl;
+  unlock = true;
+}
