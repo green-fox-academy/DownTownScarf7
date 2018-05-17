f34c175534880fadb9081e7ede256c3d9172ffe1
DownTownScarf7 <vbence741@gmail.com>
2018-05-17 12:01:22 +0200

    added rainbow-box-function.tx & .html

---
+++ week-02/day-04/rainbow-box-function.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Create a square drawing function that takes 2 parameters:
+// The square size, and the fill color,
+// and draws a square of that size and color to the center of the canvas.
+// Create a loop that fills the canvas with rainbow colored squares.
+function rndColor(): string {
+  let r = 255*Math.random()|0,
+      g = 255*Math.random()|0,
+      b = 255*Math.random()|0;
+  return `rgb(${r}, ${g}, ${b})`;
+}
+function drawSquare(size, color): void {
+  ctx.fillStyle = color;
+  ctx.fillRect(300 - size / 2, 200 - size / 2, size, size);
+
+}
+for (let i = 600; i > 0; i--) {
+  drawSquare(i, rndColor());
+}
