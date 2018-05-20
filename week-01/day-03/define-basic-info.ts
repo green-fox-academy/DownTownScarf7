4a648494b95530897cec713aa4876b0986290d4a
DownTownScarf7 <vbence741@gmail.com>
2018-05-20 14:07:29 +0200

    added missing type & newline

--- week-01/day-03/define-basic-info.ts
+++ week-01/day-03/define-basic-info.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-function dbi(name: string, age: number, height: number, marrige: boolean) {
+function dbi(name: string, age: number, height: number, marrige: boolean): string {
   return `${name}\n${age}\n${height}\n${marrige}`;
 }
 console.log(dbi('Bence Vidosits', 22, 1.75, false));
