3759afe9cbfa070ff25c85e766eb8e2f23c0a374
DownTownScarf7 <vbence741@gmail.com>
2018-05-20 14:00:14 +0200

    remvoed unnecessary newline and comment

--- week-01/day-03/two-numbers.ts
+++ week-01/day-03/two-numbers.ts
@@ -1,8 +1,7 @@
 'use strict';
 
-// (two-numbers) Doing operations on x and y
+// Doing operations on x and y
 function qikmafs(num1, num2) {
-
   return `${num1 + num2}\n${num2 - num1}\n${num1 * num2}\n${num2 / num1}\n${num2 % num1}`;
 }
 console.log(qikmafs(13, 22));
