119476cbececce74c571a47a5d73d04d5b07574e
DownTownScarf7 <vbence741@gmail.com>
2018-05-20 14:41:31 +0200

    added missing types, minor improvement

--- week-01/day-03/swap.ts
+++ week-01/day-03/swap.ts
@@ -1,17 +1,13 @@
 'use strict';
 
 // (swap) Swapping a and b like a boss
-function swap(num1, num2) {
+function swap(num1: number, num2: number): void {
   let a: number = num1;
   let b: number = num2;
 
   console.log(`${a}\n${b}`);
-  
   a = num2;
   b = num1;
-
   console.log(`${a}\n${b}`);
-
-  return true;
 }
-swap(123, 526)
+swap(123, 526);
