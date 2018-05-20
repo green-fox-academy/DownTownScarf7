d0822d71012973d7755170a628935b481d8a093e
DownTownScarf7 <vbence741@gmail.com>
2018-05-20 14:06:19 +0200

    improved code, added missing types

--- week-01/day-03/coding-hours.ts
+++ week-01/day-03/coding-hours.ts
@@ -1,14 +1,7 @@
 'use strict';
 
-// (coding-hours) Calculating...
-function codingHours(hours, weeks, awh) {
-
-// Hours spent coding in a semester
-  console.log(weeks * 5 * hours);
-
-// Percentage of working hours
-  console.log( ((weeks * awh) / (weeks * 168) * 100).toFixed(2) + '%' );
-
-  return true;
+function codingHours(hours: number, weeks: number, awh: number): void {
+// Hours spent coding in a semester, percentage of working hours
+  console.log(`${weeks * 5 * hours}\n${Math.round((weeks * awh) / (weeks * 168) * 100 * 100) / 100}%`);
 }
 codingHours(6, 17, 52);
