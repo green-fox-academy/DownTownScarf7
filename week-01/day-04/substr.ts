1ed3f8c507928c6effab8425ebc0d329096ca060
DownTownScarf7 <vbence741@gmail.com>
2018-05-20 14:43:38 +0200

    minor improvement

--- week-01/day-04/substr.ts
+++ week-01/day-04/substr.ts
@@ -3,7 +3,7 @@
 //  Create a function that takes two strings as a parameter
 //  Returns the starting index where the second one is starting in the first one
 //  Returns `-1` if the second string is not in the first one
-function substr(str: string, keyword: string) : number {
+function substr(str: string, keyword: string): number {
   return str.indexOf(keyword);
 }
 
