dc93ba3bdeb4a2b2624476eecf6850478962a5e4
DownTownScarf7 <vbence741@gmail.com>
2018-05-27 20:27:44 +0200

    fixed style, added missing semicolon, added missing types

--- week-02/day-02/matchmaking.ts
+++ week-02/day-02/matchmaking.ts
@@ -1,16 +1,16 @@
-export{};
+export { };
 'use strict';
 // Write a function that joins two array by matching one girl with one boy in a new array
 // Exepected output: ["Eve", "Joe", "Ashley", "Fred"...]
 let girls: string[] = ['Eve', 'Ashley', 'Bözsi', 'Kat', 'Jane'];
 let boys: string[] = ['Joe', 'Fred', 'Béla', 'Todd', 'Neef', 'Jeff'];
 
-function makingMatches(list1, list2){
+function makingMatches(list1: string[], list2: string[]): string[] {
   let madeInHeaven: string[] = list1;
   let step: number = 0;
   for (let i = 1; i <= (list1.length); i += 2) {
     madeInHeaven.splice(i, 0, boys[step]);
-    step++
+    step++;
   }
   return madeInHeaven;
 }
