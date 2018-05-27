1232b1a30988203e70c5e49f720b147aad01207d
DownTownScarf7 <vbence741@gmail.com>
2018-05-27 20:10:11 +0200

    broken down function into multiple lines

--- week-02/day-01/double-items.ts
+++ week-02/day-01/double-items.ts
@@ -3,5 +3,8 @@ export{};
 
 let drinks: string[] = ['Gin', 'Whiskey', 'Wine', 'Beer'];
 
-drinks = drinks.map(element => { element += element; return element;});
+drinks = drinks.map(element => {
+  element += element;
+  return element;
+});
 console.log(drinks);
