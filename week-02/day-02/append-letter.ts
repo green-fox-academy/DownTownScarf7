5e35d85e8859a14f2d40099af6d9a88b0db8f3dc
DownTownScarf7 <vbence741@gmail.com>
2018-05-27 20:20:39 +0200

    added missing spaces

--- week-02/day-02/append-letter.ts
+++ week-02/day-02/append-letter.ts
@@ -1,11 +1,11 @@
-export{};
+export { };
 'use strict';
 // Create a function called "appendA()" that adds "a" to every string in the far list.
 // The parameter should be a list.
 let far: string[] = ['kuty', 'macsk', 'kacs', 'rók', 'halacsk'];
 
-function appendA(list:string[]):string[]{
-  list = list.map(ele => {ele += 'a'; return ele;});
+function appendA(list: string[]): string[] {
+  list = list.map(ele => { ele += 'a'; return ele; });
   return list;
 }
 
