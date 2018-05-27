dfd135508a34f880dd9a6383aed21645e327164f
DownTownScarf7 <vbence741@gmail.com>
2018-05-27 18:33:55 +0200

    balance changes, dead pirates now get removed from crew, fixed bugs

--- week-03/pirate-wars/ship.ts
+++ week-03/pirate-wars/ship.ts
@@ -7,22 +7,33 @@ function rndNum(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function wait(ms: number): void {
+  let start: number = Date.now(),
+      now: number = start;
+  while (now - start < ms) {
+    now = Date.now();
+  }
+}
+
 class Ship {
   private _crew: Pirate[];
   private _id: string;
   private _health: number;
+  private _maxHealth: number;
   private _cannonNum: number;
   private _rumStock: number;
   private _sunk: boolean;
 
-  constructor(id: number) {
+  constructor(id: number, crewSize: number = 20) {
     this._crew = [];
     this._id = `Ship ${id}`;
-    this._health = 1000;
+    this._health = 5000;
+    this._maxHealth = this._health + 0;
     this._cannonNum = 20;
     this._rumStock = 3;
     this._sunk = false;
-    console.log(`${this._id} on the seven seas!`);
+    this._recruitCrew(crewSize);
+    console.log(`${this._id} on the seven seas with ${crewSize} seadogs!`);
   }
 
   public inspectCrew(): void {
@@ -33,6 +44,10 @@ class Ship {
 
   public getCrewSize(): number {
     return this._crew.length;
+  }
+
+  public isDead() {
+    return this._sunk;
   }
 
   public inspectShip(): void {
@@ -50,7 +65,7 @@ class Ship {
     }
   }
 
-  public recruitCrew(num: number): void {
+  private _recruitCrew(num: number): void {
     let temp: number = this._crew.length;
     for (let i: number = 0 + temp; i < num + temp; i++) {
       if (this._crew.length === 0) {
@@ -59,30 +74,36 @@ class Ship {
         this._crew.push(new Pirate(i, this._id));
       }
     }
-    console.log(`Yoohoohoo! ${this._id} just recruited ${num} seadogs!`);
   }
 
   public battle(target: Ship): void {
-    let self: Ship = this;
     if (this._health > 0 && this.getCrewSize() > 0) {
       if (this._cannonNum > 0) {
-        console.log(`${this._id} firing at ${target._id} with ${this._cannonNum} cannons.`);
-        if (this._health === 1000) {
-          target._fireAt(Math.floor(this._cannonNum / 2));
+        if (this._health === this._maxHealth) {
+          let temp: number = rndNum(Math.floor(this._cannonNum / 3), this._cannonNum);
+          console.log(`${this._id} engaged ${target._id} with ${temp} cannons.`);
+          target._fireAt(temp);
         } else {
+          console.log(`${this._id} firing at ${target._id} with ${this._cannonNum} cannons.`);
           target._fireAt(this._cannonNum);
         }
       } else {
         target._board();
       }
-      setTimeout(function() {
-        target.battle(self);
-      }, 1000);
-    } else if (this._health <= 0) {
+      wait(100);
+      target.battle(this);
+    } 
+  }
+
+  private _modHealth(num: number): void {
+    this._health += num;
+    if (this._health <= 0) {
       this._health = 0;
       console.log(`${this._id} just sunk!`);
-    } else {
+      this._sunk = true;
+    } else if (this.getCrewSize() <= 0){
       console.log(`${this._id}'s crew perished!`);
+      this._sunk = true;
     }
   }
 
@@ -94,9 +115,9 @@ class Ship {
 
     for (let i: number = 0; i < cannonNum; i++) {
       if (this._health > 0) {
-        let cannonDmg: number = rndNum(10, 30);
+        let cannonDmg: number = rndNum(10, 100);
         dmgCounter += cannonDmg;
-        this._health -= cannonDmg;
+        this._modHealth(-Math.abs(cannonDmg));
         if (rndNum(0, 100) < 15) {
           dstrdCannonCounter++;
           this._cannonNum--;
@@ -116,14 +137,21 @@ class Ship {
 
   private _damageCrew(numOfPirates: number, damage: number): void {
     for (let i: number = 0; i < numOfPirates; i++) {
-      this._crew[rndNum(0, this.getCrewSize() - 1)].modHealth(-Math.abs(damage));
+      if (this.getCrewSize() > 0) {
+        this._crew[rndNum(0, this.getCrewSize() - 1)].modHealth(-Math.abs(damage));
+        this._crew = this._crew.filter(pirate => {
+          return !pirate.isDead();
+        })
+      } else {
+        break;
+      }
     }
     //console.log(`${this._id}'s ${numOfPirates} crewmember(s) suffered ${damage} damage!`);
   }
 
   private _board() {
     console.log('Boarding not implemented yet!');
-    this._health -= 1000;
+    this._modHealth(-1000);
   }
 }
 
