04a8778e66a13b2b0e5e16c4f82ddedffbfd7236
DownTownScarf7 <vbence741@gmail.com>
2018-05-28 21:15:24 +0200

    removed underscores for private variables/methods

--- week-03/pirate-wars/pirates.ts
+++ week-03/pirate-wars/pirates.ts
@@ -22,7 +22,7 @@ class Pirate {
     this.health = 100;
     this.maxHealth = this.health + 0;
     this.dead = false;
-    this._checkDrunkness();
+    this.checkDrunkness();
   }
 
   public isDead(): boolean {
@@ -32,59 +32,57 @@ class Pirate {
   // To drink or not to drink
   public decideRum(): void {
     if (rndNum(0, this.drunkness) - (this.maxHealth - this.health) < 40) {
-      this._drinkRum();
+      this.drinkRum();
     }
   }
 
-  private _drinkRum(): void {
+  private drinkRum(): void {
     let temp: number = rndNum(10, 25);
     this.drunkness += temp;
     this.modHealth(temp);
-    this._checkDrunkness();
+    this.checkDrunkness();
   }
 
   // Modifies stats according to drunkness
-  private _checkDrunkness(): void {
+  private checkDrunkness(): void {
     if (this.drunkness < 20) {
-      this._modStats(-2, -2);
+      this.modStats(-2, -2);
     } else if (this.drunkness >= 20 && this.drunkness < 40) {
-      this._modStats(-1, -1);
+      this.modStats(-1, -1);
     } else if (this.drunkness >= 40 && this.drunkness < 60) {
-      this._modStats(1, 1);
+      this.modStats(1, 1);
     } else if (this.drunkness >= 60 && this.drunkness < 80) {
-      this._modStats(2, 2);
+      this.modStats(2, 2);
     } else if (this.drunkness >= 80 && this.drunkness < 95) {
-      this._modStats(5, 5);
+      this.modStats(5, 5);
       if (rndNum(0, 100) > 50) {
-        this._vomit();
+        this.vomit();
       }
     } else {
-      this._vomit();
+      this.vomit();
     }
   }
 
-  private _vomit(): void {
+  private vomit(): void {
     let temp: number = rndNum(20, 50);
     this.drunkness -= temp;
     this.modHealth(-20);
-    this._checkDrunkness();
+    this.checkDrunkness();
   }
 
   // Fights another pirate
   public fight(target: Pirate): void {
     if (this.health > 0) {
       if (target.dfs + 5 < this.atk) {
-        console.log(`${this.id} deals ${this.atk - target.dfs} damage to ${target.id}`);
         target.modHealth(-Math.abs(this.atk - target.dfs));
         if (target.health > 0) {
-          target._modStats(0, -1);
+          target.modStats(0, -1);
           target.fight(this);
         }
       } else {
         target.modHealth(-5);
-        console.log(`${this.id} deals 5 damage to ${target.id}`);
         if (target.health > 0) {
-          target._modStats(0, -2);
+          target.modStats(0, -2);
           target.fight(this);
         }
       }
@@ -102,7 +100,7 @@ class Pirate {
   }
 
   // Modifies stats
-  private _modStats(atk: number, dfs: number): void {
+  private modStats(atk: number, dfs: number): void {
     this.atk += atk;
     this.dfs += dfs;
     if (this.atk < 5) {
