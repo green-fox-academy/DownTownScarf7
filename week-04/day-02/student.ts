a84dc34c6242599be9425840e90ebba2da56551c
DownTownScarf7 <vbence741@gmail.com>
2018-05-29 11:29:10 +0200

    added student.ts, child class of Person

---
+++ week-04/day-02/student.ts
@@ -0,0 +1,28 @@
+'use strict';
+
+import { Person } from './person'
+
+class Student extends Person {
+  protected previousOrganization: string;
+  protected skippedDays: number;
+
+  public constructor(name: string = 'Jane Doe', age: number = 30, gender: string = 'female', previousOrganization: string = 'The School of Life') {
+    super(name, age, gender);
+    this.previousOrganization = previousOrganization;
+    this.skippedDays = 0;
+  }
+
+  public setGoal(): void {
+    console.log('Be a junior software developer');
+  }
+
+  public introduce(): void {
+    console.log(`Hi, I'm ${this.name}, a ${this.age} year old ${this.gender} from ${this.previousOrganization} who skipped ${this.skippedDays} days`);
+  }
+
+  public skipDays(numberofDays): void {
+    this.skippedDays += numberofDays;
+  }
+}
+
+export { Student };
