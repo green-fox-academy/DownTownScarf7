fd7ebb3b738e0c8f52969ed98eb1b7cd14f872ed
DownTownScarf7 <vbence741@gmail.com>
2018-05-29 13:59:03 +0200

    added garden.ts

---
+++ week-04/day-02/garden.ts
@@ -0,0 +1,47 @@
+'use strict';
+
+import { Plant } from './plant';
+import { Flower } from './flower';
+import { Tree } from './tree';
+
+class Garden {
+  private plants: any[];
+
+  public constructor(numFlowers: number = 0, numTrees: number = 0) {
+    this.plants = [];
+    this.initGarden(numFlowers, numTrees);
+  }
+
+  public initGarden(numFlowers: number, numTrees: number): void {
+    for (let i: number = 0; i <= numFlowers; i++) {
+      this.plants.push(new Flower('red'));
+    }
+    for (let i: number = 0; i <= numTrees; i++) {
+      this.plants.push(new Tree('green'));
+    }
+  }
+
+  public addFlower(color: string): void {
+    this.plants.push(new Flower(color));
+  }
+
+  public addTree(color: string): void {
+    this.plants.push(new Tree(color));
+  }
+
+  public checkGarden(): void {
+    for (let i: number = 0; i < this.plants.length; i++) {
+      this.plants[i].checkPlant();
+    }
+  }
+
+  public water(amount: number): void {
+    console.log(`\nWatering with ${amount}`)
+    for (let i: number = 0; i < this.plants.length; i++) {
+      this.plants[i].water();
+    }
+    this.checkGarden();
+  }
+}
+
+export { Garden };
