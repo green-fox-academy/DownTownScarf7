36c0111238d33ddb262abdba9a4731cbfae2d522
DownTownScarf7 <vbence741@gmail.com>
2018-05-29 11:43:38 +0200

    added cohort.ts

---
+++ week-04/day-02/cohort.ts
@@ -0,0 +1,28 @@
+'use strict';
+
+import { Student } from './student';
+import { Mentor } from './mentor';
+
+class Cohort {
+  protected name: string;
+  protected students: Student[];
+  protected mentors: Mentor[];
+
+  public constructor(name: string) {
+    this.name = name;
+    this.students = [];
+    this.mentors = [];
+  }
+
+  public addStudent(student: Student): void {
+    this.students.push(student);
+  }
+
+  public addMentor(mentor: Mentor): void {
+    this.mentors.push(mentor);
+  }
+
+  public info(): void {
+    console.log(`The ${this.name} cohort has ${this.students.length} students and ${this.mentors.length} mentors.`);
+  }
+}
