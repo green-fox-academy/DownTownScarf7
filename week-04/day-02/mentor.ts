863a3ba943264590b89d07fe3642fbb5d8e62df9
DownTownScarf7 <vbence741@gmail.com>
2018-05-29 11:29:35 +0200

    added mentor.ts, child class of Person

---
+++ week-04/day-02/mentor.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+import { Person } from './person';
+
+class Mentor extends Person {
+  protected level: string;
+
+  public constructor(name: string = 'Jane Doe', age: number = 30, gender: string = 'female', level: string = 'intermediate') {
+    super(name, age, gender);
+    this.level = level;
+  }
+
+  public setGoal(): void {
+    console.log('Educate brilliant junior software developers.');
+  }
+
+  public introduce(): void {
+    console.log(`Hi, I'm ${this.name}, a ${this.age} year old ${this.gender} ${this.level} mentor.`);
+  }
+}
