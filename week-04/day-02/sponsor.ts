b94b5e9989fccc780a7e1c69bd6caad7e441ba4a
DownTownScarf7 <vbence741@gmail.com>
2018-05-29 11:32:45 +0200

    added sponsor.ts, child class of Person

---
+++ week-04/day-02/sponsor.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+import { Person } from './person';
+
+class Sponsor extends Person {
+  protected company: string;
+  protected hiredStudents: number;
+
+  public constructor(name: string = 'Jane Doe', age: number = 30, gender: string = 'female', company: string = 'Google') {
+    super(name, age, gender);
+    this.company = company;
+    this.hiredStudents = 0;
+  }
+
+  public introduce(): void {
+    console.log(`Hi, I'm ${this.name}, a ${this.age} year old ${this.gender} who represents ${this.company} and hired ${this.hiredStudents} students so far.`);
+  }
+
+  public hire(): void {
+    this.hiredStudents++;
+  }
+
+  public setGoal(): void {
+    console.log('Hire brilliant junior software developers.');
+  }
+}
