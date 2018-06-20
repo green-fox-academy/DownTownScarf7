6f0ecaf128e4bc356e9158c102179c40f6850868
DownTownScarf7 <vbence741@gmail.com>
2018-06-20 11:42:56 +0200

    added missing task

--- week-06/day-03/queryselector-1.js
+++ week-06/day-03/queryselector-1.js
@@ -3,10 +3,14 @@
 const king = document.querySelector('#b325');
 const businessLamp = document.querySelectorAll('.big');
 const conceitedKing = document.querySelectorAll('.container div');
+const noBusiness = document.querySelectorAll('div');
 console.log(king);
 businessLamp.forEach(elem => {
   console.log(elem);
 });
 conceitedKing.forEach(elem => {
+  console.log(elem);
+});
+noBusiness.forEach(elem => {
   console.log(elem);
 });
