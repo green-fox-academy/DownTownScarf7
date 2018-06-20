9e4a9d0b847867c7a1051df0b17bba183290ce28
DownTownScarf7 <vbence741@gmail.com>
2018-06-20 11:04:39 +0200

    added queryselector-1.html and .js

---
+++ week-06/day-03/queryselector-1.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const king = document.querySelector('#b325');
+const businessLamp = document.querySelectorAll('.big');
+const conceitedKing = document.querySelectorAll('.container div');
+console.log(king);
+businessLamp.forEach(elem => {
+  console.log(elem);
+});
+conceitedKing.forEach(elem => {
+  console.log(elem);
+});
