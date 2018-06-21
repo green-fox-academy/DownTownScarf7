66b12d94d2ba9d4643720b5d94ff3fe718746748
DownTownScarf7 <vbence741@gmail.com>
2018-06-21 11:02:33 +0200

    added extra requests

--- week-06/day-04/server.js
+++ week-06/day-04/server.js
@@ -8,6 +8,23 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/hola', (req, res) => {
+  res.send('Hola!');
+});
+
+app.get('/greet/:message/', (req, res) => {
+  console.log(req.query);
+  let name = 'Fox';
+  if (req.query.name !== undefined) {
+    name = req.query.name;
+  }
+  res.send(`${req.params.message}, ${req.query.name}`);
+});
+
+app.get('/cars/:type/', (req, res) => {
+  res.send(req.params.type);
+});
+
 app.listen(PORT, () => {
   console.log(`Yey, I'm running on port ${PORT}`);
 });
