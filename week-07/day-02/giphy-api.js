0d8a543ca1f9d4844a3f6215e7db9cee950f7387
DownTownScarf7 <vbence741@gmail.com>
2018-06-26 17:10:14 +0200

    added inputs for webpage

--- week-07/day-02/giphy-api.js
+++ week-07/day-02/giphy-api.js
@@ -6,17 +6,24 @@ httpRequest.onload = () => {
   let res = JSON.parse(httpRequest.responseText);
 
   res.data.forEach(elem => {
-    document.querySelector('section').appendChild(document.createElement('img')).setAttribute('src', elem.images.original_still.url);
+    document.querySelector('section').appendChild(document.createElement('img')).setAttribute('src', elem.images.fixed_width_still.url);
   });
   document.querySelectorAll('img').forEach((elem, index) => {
     elem.addEventListener('click', () => {
-      elem.setAttribute('src', res.data[index].images.original.url);
+      elem.setAttribute('src', res.data[index].images.fixed_width.url);
     });
   });
   
 };
 document.querySelector('button').onclick = () => {
-  document.querySelector('button').setAttribute('disabled', null);
-  httpRequest.open('GET', `https://api.giphy.com/v1/gifs/search?api_key=FnNNMzbs5Tzy717siUEsfZiKvJfbpgdC&q=${document.querySelector('input').value ? document.querySelector('input').value : 'ERROR'}&limit=16&offset=0&rating=G&lang=en`, true);
+  const pictures = document.querySelectorAll('img'),
+        tag = document.querySelector('#searchTag').value || 'error',
+        limit = document.querySelector('#searchLimit').value || 1;
+  if (pictures) {
+    pictures.forEach((elem, index) => {
+      document.querySelector('section').removeChild(pictures[index]);
+    });
+  }
+  httpRequest.open('GET', `https://api.giphy.com/v1/gifs/search?api_key=FnNNMzbs5Tzy717siUEsfZiKvJfbpgdC&q=${tag}&limit=${limit}&offset=0&rating=G&lang=en`, true);
   httpRequest.send();
 }
