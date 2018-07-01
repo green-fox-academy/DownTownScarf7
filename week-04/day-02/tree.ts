41253b159c1d5ef274cd194cca65aa079108d467
DownTownScarf7 <vbence741@gmail.com>
2018-07-01 12:18:32 +0200

    added missing type

--- week-04/day-02/tree.ts
+++ week-04/day-02/tree.ts
@@ -2,8 +2,8 @@
 
 import { Plant } from './plant';
 
-class Tree extends Plant {
-  public constructor(color) {
+export class Tree extends Plant {
+  public constructor(color: string) {
     super(color);
     this.threshold = 10;
     this.id = 'tree';
@@ -13,5 +13,3 @@ class Tree extends Plant {
     super.mainWater(amount * 0.40);
   }
 }
-
-export { Tree };
