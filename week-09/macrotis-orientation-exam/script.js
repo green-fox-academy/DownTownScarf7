11a22101594057393c4427b3ff4c00e7113ee57e
DownTownScarf7 <vbence741@gmail.com>
2018-07-10 15:45:35 +0200

    added basic table printing and edit button

--- week-09/macrotis-orientation-exam/script.js
+++ week-09/macrotis-orientation-exam/script.js
@@ -3,6 +3,19 @@
 const http = new XMLHttpRequest();
 
 window.onload = () => {
+  document.querySelector('table').addEventListener('click', event => {
+    if (event.target.classList.contains('btn')) {
+      http.open('GET', `http://localhost:3000/api/attractions/${event.target.dataset.id}`);
+      http.onload = () => {
+        const { attr_name, city, price, longitude, lattitude, category, duration, recommended_age } = JSON.parse(http.responseText).attractions[0];
+        [attr_name, city, price, longitude, lattitude, category, duration, recommended_age].forEach((element, index) => {
+          document.querySelectorAll('input')[index].value = element;
+        });
+      }
+      http.send();
+    }
+  });
+  
   http.open('GET', 'http://localhost:3000/api/attractions', true);
   http.setRequestHeader("Content-Type", "application/json");
   http.onload = () => {
@@ -15,13 +28,20 @@ window.onload = () => {
       return;
     }
     if (res) {
-      const table = document.querySelector('table');
-      res.forEach(element => {
-        const row = document.createElement('tr');
-        row.appendChild(document.createElement('td'));
+      res.forEach((element, index) => {
+        const { attr_name, city, price, longitude, lattitude, category, duration, recommended_age } = element;
+        const tr = document.createElement('tr');
+        const btnEdit = document.createElement('button');
+        btnEdit.dataset.id = (index + 1);
+        btnEdit.classList.add('btn', 'edit');
+        btnEdit.innerHTML = 'Edit';
+        document.querySelector('tbody').appendChild(tr);
+        [attr_name, city, price, longitude, lattitude, category, duration, recommended_age].forEach(attr => {
+          tr.appendChild(document.createElement('td')).innerHTML = attr;
+        });
+        tr.appendChild(btnEdit);
       });
     }
-    console.log(res);
   }
   http.send();
 }
