b6b28a8a2e0c3029d85283826ca1db26a36094be
DownTownScarf7 <vbence741@gmail.com>
2018-07-11 10:29:22 +0200

    added matetrial review

---
+++ week-09/day-03/mat-rev/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// const promise = Promise.resolve(42);
+// console.log(promise);
+
+// promise.then(resolved => {
+//   console.log(resolved);
+// });
+
+// const promise2 = Promise.reject(42);
+// console.log(promise2);
+
+// promise2.then(resolved => {
+//   console.log('Resolved!');
+// }).catch(resolved => {
+//   console.log('Rejected!');
+// });
+
+// const promise3 = new Promise((resolve, reject) => {
+//   resolve(true);
+// });
+
+// promise3.then(value => {
+//   console.log(value);
+//});
+
+// const promise4 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('RESOLVED');
+//   }, 1000);
+// });
+
+// promise4.then((resolved) => {
+//   return resolved.split('');
+// }).then(resolvedSplit => {
+//   return `${resolvedSplit[0]}${resolvedSplit[resolvedSplit.length - 1]}`;
+// }).then(resolvedCut => {
+//   console.log(resolvedCut);
+// });
+
+
